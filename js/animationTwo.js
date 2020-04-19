6527ef0e425b9ea6e2176293ce458d387828f2ea
farith comas <farith.comas@gmail.com>
2020-04-18 21:59:12 -0500

    cahnge url

--- js/animationTwo.js
+++ js/animationTwo.js
@@ -33,13 +33,13 @@ window.addEventListener("load", () => {
   // Async function to fetch the data from the server as a json file
   // Fetching the name data
   const requestName = async () => {
-    const responseName = await fetch("http://192.168.0.12:3000/name");
+    const responseName = await fetch("http://127.0.0.1:3000/name");
     dataName = await responseName.json();
   }
 
   // Fetching the skills data
   const requestSkills = async () => {
-    const responseSkills = await fetch("http://192.168.0.12:3000/skills");
+    const responseSkills = await fetch("http://127.0.0.1:3000/skills");
     skills = await responseSkills.json();
   }
 
